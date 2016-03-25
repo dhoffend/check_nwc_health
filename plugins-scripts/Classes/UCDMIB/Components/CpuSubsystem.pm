e2a773ce29cc1633b910b7ee5acc58d5fe3a85e0
Daniel Hoffend <dh@dotlan.net>
2016-03-25 21:37:29 +0100

    added RawWait, RawKernel, RawInterrupt to cpu_usage calculation

--- plugins-scripts/Classes/UCDMIB/Components/CpuSubsystem.pm
+++ plugins-scripts/Classes/UCDMIB/Components/CpuSubsystem.pm
@@ -5,10 +5,18 @@ use strict;
 sub init {
   my $self = shift;
   $self->get_snmp_objects('UCD-SNMP-MIB', (qw(
-      ssCpuUser ssCpuSystem ssCpuIdle ssCpuRawUser ssCpuRawSystem ssCpuRawIdle ssCpuRawNice)));
-  $self->valdiff({name => 'cpu'}, qw(ssCpuRawUser ssCpuRawSystem ssCpuRawIdle ssCpuRawNice));
-  my $cpu_total = $self->{delta_ssCpuRawUser} + $self->{delta_ssCpuRawSystem} +
-      $self->{delta_ssCpuRawIdle} + $self->{delta_ssCpuRawNice};
+      ssCpuUser ssCpuSystem ssCpuIdle ssCpuRawUser ssCpuRawSystem ssCpuRawIdle
+      ssCpuRawNice ssCpuRawWait ssCpuRawKernel ssCpuRawInterrupt)));
+  $self->valdiff({name => 'cpu'}, qw(
+      ssCpuRawUser ssCpuRawSystem ssCpuRawIdle ssCpuRawNice ssCpuRawWait
+      ssCpuRawKernel ssCpuRawInterrupt));
+  my $cpu_total = 0;
+  # not every kernel/snmpd supports every counters
+  foreach (qw(delta_ssCpuRawUser delta_ssCpuRawSystem delta_ssCpuRawIdle
+    delta_ssCpuRawNice delta_ssCpuRawWait delta_ssCpuRawKernel
+    delta_ssCpuRawInterrupt)) {
+    $cpu_total += $self->{$_} if defined($self->{$_});
+  }
   if ($cpu_total == 0) {
     $self->{cpu_usage} = 0;
   } else {
