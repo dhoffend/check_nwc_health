a1b421b4a01e3bf3c8a952d6d7374fa414083bde
Daniel Hoffend <dh@dotlan.net>
2016-03-26 00:27:32 +0100

    fixed memory usage calculation for ucdmib, added swap usage to memory-usage

--- plugins-scripts/Classes/UCDMIB/Components/MemSubsystem.pm
+++ plugins-scripts/Classes/UCDMIB/Components/MemSubsystem.pm
@@ -5,11 +5,23 @@ use strict;
 sub init {
   my $self = shift;
   $self->get_snmp_objects('UCD-SNMP-MIB', (qw(
-      memTotalSwap memAvailSwap memTotalReal memAvailReal memTotalFree)));
+      memTotalSwap memAvailSwap memTotalReal memAvailReal
+      memBuffer memCached)));
+
+  # basically buffered memory can always be freed up (filesystem cache)
   # https://kc.mcafee.com/corporate/index?page=content&id=KB73175
-  $self->{mem_usage} = ($self->{memTotalReal} - $self->{memTotalFree}) /
-      $self->{memTotalReal} * 100;
-  $self->{mem_usage} = $self->{memAvailReal} * 100 / $self->{memTotalReal};
+  my $mem_available = $self->{memAvailReal};
+  foreach (qw(memBuffer memCached)) {
+    $mem_available += $self->{$_} if defined($self->{$_});
+  }
+
+  # calc memory (no swap)
+  $self->{mem_usage} = 100 - ($mem_available * 100 / $self->{memTotalReal});
+
+  # calc swap usage
+  if (defined $self->{memAvailSwap} && defined $self->{memTotalSwap}) {
+    $self->{swap_usage} = 100 - ($self->{memAvailSwap} * 100 / $self->{memTotalSwap});
+  }
 }
 
 sub check {
@@ -28,6 +40,19 @@ sub check {
     );
   } else {
     $self->add_unknown('cannot aquire memory usage');
+  }
+
+  if (defined $self->{'swap_usage'}) {
+    $self->add_info(sprintf 'swap usage is %.2f%%',
+        $self->{swap_usage});
+    $self->set_thresholds(warning => 10,
+        critical => 50);
+    $self->add_message($self->check_thresholds($self->{swap_usage}));
+    $self->add_perfdata(
+        label => 'swap_usage',
+        value => $self->{swap_usage},
+        uom => '%',
+    );
   }
 }
 
