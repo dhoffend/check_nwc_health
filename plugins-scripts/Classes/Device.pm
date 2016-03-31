f8036dde7a2136413cfb2776038923c06aefb6cf
Daniel Hoffend <dh@dotlan.net>
2016-04-01 01:00:53 +0200

    removed trailing whitespace in regexp

--- plugins-scripts/Classes/Device.pm
+++ plugins-scripts/Classes/Device.pm
@@ -181,7 +181,7 @@ sub classify {
       } elsif ($self->{sysobjectid} =~ /1\.3\.6\.1\.4\.1\.9\./) {
         bless $self, 'Classes::Cisco';
         $self->debug('using Classes::Cisco');
-      } elsif ($self->{productname} =~ /^Linux /i) {
+      } elsif ($self->{productname} =~ /^Linux/i) {
         bless $self, 'Classes::Server::Linux';
         $self->debug('using Classes::Server::Linux');
       } else {
